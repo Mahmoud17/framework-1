a938ffe250611f3fa5d78d920b17f15a3a3fad8e
Mahmoud Ayoub <mahmoud30ayoub@gmail.com>
2023-09-13 10:50:57 +0300

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import {User} from './user'
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  color = true
+  names: string[] = ['mo', 'ahmed', 'omar']
+  users: User[] = [
+    {
+      name: 'mostafa',
+      age: 25,
+      salary: 20000,
+      gender: 'male',
+      work: function () {
+        alert('welcome  ' + this.name);
+      },
+    },
+
+    {
+      name: 'mahmod',
+      age: 25,
+      salary: 20000,
+      gender: 'male',
+      work: function () {
+        alert('welcome  ' + this.name);
+      },
+    },
+    {
+      name: 'salma',
+      age: 25,
+      salary: 20000,
+      gender: 'fmale',
+      work: function () {
+        alert('welcome  ' + this.name);
+      },
+    },
+    {
+      name: 'rehab',
+      age: 25,
+      salary: 20000,
+      gender: 'female',
+      work: function () {
+        alert('welcome  ' + this.name);
+      },
+    },
+    {
+      name: 'gehad',
+      age: 25,
+      salary: 20000,
+      gender: 'female',
+      work: function () {
+        alert('welcome  ' + this.name);
+      },
+    },
+  ];
+  toggleColor() {
+    this.color = !this.color
+  }
+  method() {
+    return "Hello this is a " + this.title + " App"
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './todo/todo.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { NestedComponent } from './comps/nested/nested.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    NestedComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
